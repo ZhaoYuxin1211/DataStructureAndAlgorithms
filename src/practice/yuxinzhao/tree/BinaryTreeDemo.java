746180fa73aad9681e27cec24be9093376d23f36
ZhaoYuxin1211 <zhaoyuxin97@foxmail.com>
2024-01-24 18:51:39 +0000

    Binary Tree(search, delete)

--- src/practice/yuxinzhao/tree/BinaryTreeDemo.java
+++ src/practice/yuxinzhao/tree/BinaryTreeDemo.java
@@ -23,41 +23,80 @@ public class BinaryTreeDemo {
 
         System.out.println("Postorder traversal:");
         binaryTree.postOrder();
+
+        // search
+        Node result = binaryTree.preOrderSearch(3);
+        System.out.println(result);
+
     }
 }
 
-class BinaryTree{
+class BinaryTree {
     private Node root;
 
-    public void setRoot(Node root){
+    public void setRoot(Node root) {
         this.root = root;
     }
 
-    public void preOrder(){
-        if (this.root != null){
+    public void preOrder() {
+        if (this.root != null) {
             this.root.preOrder();
-        }else{
+        } else {
             System.out.println("The current Binary Tree is null.");
         }
     }
 
-    public void inOrder(){
-        if (this.root != null){
+    public void inOrder() {
+        if (this.root != null) {
             this.root.inOrder();
-        }else {
+        } else {
             System.out.println("The current Binary Tree is null.");
         }
     }
 
-    public void postOrder(){
-        if (this.root != null){
+    public void postOrder() {
+        if (this.root != null) {
             this.root.postOrder();
-        }else {
+        } else {
             System.out.println("The current Binary Tree is null.");
         }
     }
 
+    public Node preOrderSearch(int id) {
+        if (root != null) {
+            return root.preOrderSearch(id);
+        } else {
+            return null;
+        }
+    }
 
+    public Node inOrderSearch(int id) {
+        if (root != null) {
+            return root.inOrderSearch(id);
+        } else {
+            return null;
+        }
+    }
+
+    public Node postOrderSearch(int id) {
+        if (root != null) {
+            return root.postOrderSearch(id);
+        } else {
+            return null;
+        }
+    }
+
+    public void delNode(int id){
+        if (root != null){
+            if (root.getId() == id){
+                root = null;
+            }else {
+                root.delNode(id);
+            }
+        }else {
+            System.out.println("This tree is null.");
+        }
+    }
 }
 
 class Node {
@@ -130,9 +169,9 @@ class Node {
         }
     }
 
-    public void inOrder(){
+    public void inOrder() {
         // left
-        if (this.left != null){
+        if (this.left != null) {
             this.left.inOrder();
         }
 
@@ -140,23 +179,117 @@ class Node {
         System.out.println(this);
 
         // right
-        if (this.right != null){
+        if (this.right != null) {
             this.right.inOrder();
         }
     }
 
-    public void postOrder(){
+    public void postOrder() {
         // left
-        if (this.left != null){
+        if (this.left != null) {
             this.left.postOrder();
         }
 
         // right
-        if (this.right != null){
+        if (this.right != null) {
             this.right.postOrder();
         }
 
         // parent
         System.out.println(this);
+    }
+
+    // Preorder traversal search
+
+    /**
+     * @param id find node by its id
+     * @return if found return node, otherwise return null
+     */
+    public Node preOrderSearch(int id) {
+        // parent
+        if (this.id == id) {
+            return this;
+        }
+
+        // left
+        Node result = null;
+        if (this.left != null) {
+            result = preOrderSearch(id);
+        }
+        if (result != null) {
+            return result;
+        }
+
+        // right
+        if (this.right != null) {
+            result = preOrderSearch(id);
+        }
+        return result;
+    }
+
+    public Node inOrderSearch(int id) {
+        Node result = null;
+
+        // left
+        if (this.left != null) {
+            result = inOrderSearch(id);
+        }
+        if (result != null) {
+            return result;
+        }
+
+        // parent
+        if (this.id == id) {
+            return this;
+        }
+
+        // right
+        if (this.right != null) {
+            result = inOrderSearch(id);
+        }
+        return result;
+    }
+
+    public Node postOrderSearch(int id) {
+        // left
+        Node result = null;
+        if (this.left != null) {
+            result = postOrderSearch(id);
+        }
+        if (result != null) {
+            return result;
+        }
+
+        // right
+        if (this.right != null) {
+            result = postOrderSearch(id);
+        }
+        if (result != null) {
+            return result;
+        }
+
+        // parent
+        if (this.id == id) {
+            return this;
+        }
+
+        return result;
+    }
+
+    public void delNode(int id){
+        if (this.left != null & this.left.id == id){
+            this.left = null;
+            return;
+        }
+        if (this.right != null & this.right.id == id){
+            this.right = null;
+            return;
+        }
+        if (this.left != null){
+            this.left.delNode(id);
+        }
+        if (this.right != null){
+            this.right.delNode(id);
+        }
     }
 }
